cdb9ee52b583ee4ba094d5bfcf57d8a5ded42a52
Novak <novaldix@gmail.com>
2023-08-09 16:35:55 +0300

    22_6_1 added functionality

--- main.cpp
+++ main.cpp
@@ -8,53 +8,55 @@ using std::endl;
 using std::string;
 using std::vector;
 
-// Изменить одну запись. Возвращает false, если записи по указанному ключу не обнаружено
-bool changeEntries(std::map<string, string> &entries, const string &key, const string &value) {
-    if (entries.count(key) == 0) return false;
+enum class opType { add, edit, remove, retrieve };
 
-    auto it = entries.find(key);
+// Изменить одну запись. Возвращает false, если записи по указанному ключу не обнаружено
+bool changeEntries(const string &key, const string &value, std::map<string, string> &out) {
+    if (out.count(key) == 0) return false;
+
+    auto it = out.find(key);
     it->second = value;
 
     return true;
 }
 
 // Добавить одну запись. Возвращает false, если записи по указанному ключу не обнаружено
-bool addToEntries(std::map<string, string> &entries, const string &key, const string &value) {
-    if (entries.count(key) == 1) return false;
+bool addToEntries(const string &key, const string &value, std::map<string, string> &out) {
+    if (out.count(key) == 1) return false;
 
-    entries.insert(std::make_pair(key, value));
+    out.insert(std::make_pair(key, value));
 
     return true;
 }
 
 // Удалить одну запись. Возвращает false, если записи по указанному ключу не обнаружено
-bool deleteFromEntries(const string &key, std::map<string, string> &from) {
-    const auto it = from.find(key);
+bool deleteFromEntries(const string &key, std::map<string, string> &out) {
+    const auto it = out.find(key);
 
-    if (it == from.end()) return false;
+    if (it == out.end()) return false;
 
-    from.erase(it);
+    out.erase(it);
 
     return true;
 }
 
 // Вернуть одну запись. Возвращает false, если записи по указанному ключу не обнаружено
-bool retrieveValueByKey(string &out, const string &key, const std::map<string, string> &from) {
-    const auto it = from.find(key);
+bool retrieveValueByKey(string &target, const string &key, const std::map<string, string> &source) {
+    const auto it = source.find(key);
 
-    if (it == from.end()) return false;
+    if (it == source.end()) return false;
 
-    out = it->second;
+    target = it->second;
 
     return true;
 }
 
 // Вернуть список значений по запрашиваемому значение. Возвращает количество обнаруженных совпадений
-int retrieveKeysByValue(vector<string> &out, const string &desiredValue, const std::map<string, string> &from) {
+int retrieveKeysByValue(vector<string> &target, const string &desiredValue, const std::map<string, string> &source) {
     int found = 0;
-    for (const auto &[key, value] : from)
+    for (const auto &[key, value] : source)
         if (value == desiredValue) {
-            out.emplace_back(key);
+            target.emplace_back(key);
             ++found;
         }
 
@@ -62,45 +64,56 @@ int retrieveKeysByValue(vector<string> &out, const string &desiredValue, const s
 }
 
 // Выводит отчет по записям
-void displayEntries(const std::map<string, string> &entries) {
+void displayEntries(const std::map<string, string> &source) {
     cout << "Отчет:" << endl;
-    for (const auto &[key, value] : entries) cout << key << ": " << value << endl;
+    for (const auto &[key, value] : source) cout << key << ": " << value << endl;
 }
 
-bool changeEntriesExtended(std::map<string, string> &out, const string &key, const string &value) {
-    if (changeEntries(out, key, value)) return true;
+// В зависимости от операции выводит на дисплей результат
+void operationReport(opType mode, bool status, const string &keyword, int itemsAmount = 0) {
+    vector<string> operationNames = { "Добавление", "Изменение", "Удаление", "Извлечение" };
+    const char* operation = operationNames[static_cast<int>(mode)].c_str();
+    const char* msg = "%s по ключу %s %s";
+    const char* onSuccess = "прошло успешно.\n";
+    string onSuccessExtended = "прошло успешно. Количество обработанных записей: " + std::to_string(itemsAmount) + "\n";
+    vector<string> causes = { "такая запись уже существует", "такой записи не существует"};
+    string onFailure = "не удалось, т.к. " + ((mode == opType::add) ? causes[0] : causes[1]) + "\n";
 
-    cout << "Изменение не удалось, т.к. записи с таким ключом (" << key << ") не существует" << endl;
-    return false;
+    if (status) {
+        if (itemsAmount == 0) printf(msg, operation, keyword.c_str(), onSuccess);
+        else printf(msg, operation, keyword.c_str(), onSuccessExtended.c_str());
+    }
+    else printf(msg, operation, keyword.c_str(), onFailure.c_str());
 }
 
-bool addToEntriesExtended(std::map<string, string> &entries, const string &key, const string &value) {
-    if (addToEntries(entries, key, value)) return true;
-
-    cout << "Добавление не удалось, т.к. запись с таким ключом (" << key << ") уже существует" << endl;
-    return false;
+bool addToEntriesExtended(const string &key, const string &value, std::map<string, string> &out) {
+    auto status = addToEntries(key, value, out);
+    operationReport(opType::add, status, key);
+    return status;
 }
 
-bool deleteFromEntriesExtended(const string &key, std::map<string, string> &from) {
-    if (deleteFromEntries(key, from)) return true;
+bool changeEntriesExtended(const string &key, const string &value, std::map<string, string> &out) {
+    auto status = changeEntries(key, value, out);
+    operationReport(opType::edit, status, key);
+    return status;
+}
 
-    cout << "Удаление не удалось, т.к. записи с таким ключом (" << key << ") не существует" << endl;
-    return false;
+bool removeFromEntriesExtended(const string &key, std::map<string, string> &out) {
+    auto status = deleteFromEntries(key, out);
+    operationReport(opType::remove, status, key);
+    return status;
 }
 
 bool retrieveValueByKeyExtended(string &out, const string &key, const std::map<string, string> &from) {
-    if (retrieveValueByKey(out, key, from)) return true;
-
-    cout << "Извлечение не удалось, т.к. записи с таким ключом (" << key << ") не существует" << endl;
-    return false;
+    auto status = retrieveValueByKey(out, key, from);
+    operationReport(opType::retrieve, status, key);
+    return status;
 }
 
 int retrieveKeysByValueExtended(vector<string> &out, const string &desiredValue, const std::map<string, string> &from) {
-    int result = retrieveKeysByValue(out, desiredValue, from);
-    if (result) return result;
-
-    cout << "Извлечение не удалось, т.к. записей с таким значением (" << desiredValue << ") не существует" << endl;
-    return false;
+    int itemsAmount = retrieveKeysByValue(out, desiredValue, from);
+    operationReport(opType::retrieve, itemsAmount > 0, desiredValue, itemsAmount);
+    return itemsAmount;
 }
 
 int main() {
@@ -113,21 +126,21 @@ int main() {
             { "12-15", "Petrov" },
     };
 
-    addToEntriesExtended(phonebook, "12-18", "Avanesov");
-    addToEntriesExtended(phonebook, "12_16", "Smirnov");
-    addToEntriesExtended(phonebook, "12-13", "Ivanov");
-    addToEntriesExtended(phonebook, "23-23", "Ivanov");
-    addToEntriesExtended(phonebook, "12-11", "Petrosian");
+    addToEntriesExtended("12-18", "Avanesov", phonebook);
+    addToEntriesExtended("12_16", "Smirnov", phonebook);
+    addToEntriesExtended("12-13", "Ivanov", phonebook);
+    addToEntriesExtended("23-23", "Ivanov", phonebook);
+    addToEntriesExtended("12-11", "Petrosian", phonebook);
 
     displayEntries(phonebook);
 
-    changeEntriesExtended(phonebook, "12-18", "Avanesian");
-    changeEntriesExtended(phonebook, "13-18", "Bykov");
+    changeEntriesExtended("12-18", "Avanesian", phonebook);
+    changeEntriesExtended("13-18", "Bykov", phonebook);
 
     displayEntries(phonebook);
 
-    deleteFromEntriesExtended( "12-18", phonebook);
-    deleteFromEntriesExtended("14-18", phonebook);
+    removeFromEntriesExtended( "12-18", phonebook);
+    removeFromEntriesExtended("14-18", phonebook);
 
     displayEntries(phonebook);
 
